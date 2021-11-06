605706739f352d8b647fec834b54f03bc6b4b20e
Anastasia Kolchina <60008375+netwasted@users.noreply.github.com>
2021-11-06 18:06:53 +0300

    R code for Pie Chart

---
+++ src/lib.R
@@ -0,0 +1,11 @@
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)   # replace_na
+library(tibble)  # column_to_rownames
+
+###
+
+DATA_DIR <- 'C:/Users/Anastasia/Documents/bio/filtered/'
+OUT_DIR <- 'C:/Users/Anastasia/Documents/bio/output/'
+
